f37c87d8a35879d80086de1e749cbf49abb81f6e
RyanMYang <32602114+ryanmyang@users.noreply.github.com>
2025-04-22 13:48:57 -0700

    count leading zeroes to set exponent

--- lab2/FPCVT.v
+++ lab2/FPCVT.v
@@ -6,6 +6,8 @@ module FPCVT(
 );
 
     reg [11:0] magnitude;
+    wire [3:0] leading_zeroes;
+    
     always @(*) begin
         S = D[11];
         //If D is -2048, hardcode the conversion from two's complement to floating point
@@ -17,18 +19,36 @@ module FPCVT(
 
         else begin // everything else
 
-        // If positive keep mag, if negative, take 2's complement
+            // If positive keep mag, if negative, take 2's complement
             if (S == 1'b0) begin
                 magnitude = D;
             end else begin
                 magnitude = ~D + 1;
             end
 
-            
+            // Count leading zeroes
+            E = count_leading_zeroes_set_E(magnitude);
+
+
+
 
         end
 
 
     end
+
+
+    function automatic [2:0] count_leading_zeroes_set_E(input [11:0] magnitude);
+    begin
+        if (magnitude[10])      count_leading_zeroes_set_E = 3'd7; // 1 leading 0
+        else if (magnitude[9])  count_leading_zeroes_set_E = 3'd6;
+        else if (magnitude[8])  count_leading_zeroes_set_E = 3'd5;
+        else if (magnitude[7])  count_leading_zeroes_set_E = 3'd4;
+        else if (magnitude[6])  count_leading_zeroes_set_E = 3'd3;
+        else if (magnitude[5])  count_leading_zeroes_set_E = 3'd2;
+        else if (magnitude[4])  count_leading_zeroes_set_E = 3'd1; // 7 leading 0s
+        else count_leading_zeroes_set_E = 3'd0; // 8 leading 0s or more
+    end
+    endfunction
 
 endmodule
