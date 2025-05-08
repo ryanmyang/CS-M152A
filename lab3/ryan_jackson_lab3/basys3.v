1cbc8c2b15298d6328ffb215141cf072a2b8d463
RyanMYang <32602114+ryanmyang@users.noreply.github.com>
2025-05-08 13:44:08 -0700

    Update basys3.v

--- lab3/ryan_jackson_lab3/basys3.v
+++ lab3/ryan_jackson_lab3/basys3.v
@@ -1,146 +1,44 @@
 module basys3 (/*AUTOARG*/
    // Outputs
-   RsTx, led,
+   output [6:0] seg,
+//   output reg  dp,
+   output [3:0] an,
+   
    // Inputs
-   RsRx, sw, btnS, btnR, clk
+   input wire [1:0] sw, 
+   input wire btnS, 
+   input wire btnR, 
+   input wire clk
    );
-
-`include "seq_definitions.v"
    
-   // USB-UART
-   input        RsRx;
-   output       RsTx;
-
-   // Misc.
-   input  [7:0] sw;
-   output [7:0] led;
-   input        btnS;                 // single-step instruction
-   input        btnR;                 // arst
+   wire clk_1hz;
+  wire clk_2hz;
+   wire clk_100hz;
+   wire clk_4hz;
+   clock clock_module (.clk(clk), .clk_1hz(clk_1hz), .clk_2hz(clk_2hz), .clk_100hz(clk_100hz), .clk_4hz(clk_4hz));
    
-   // Logic
-   input        clk;                  // 100MHz
+wire [3:0] dig3;
+    wire [3:0] dig2;
+    wire [3:0] dig1;
+    wire [3:0] dig0;
+    reg enable;
+    reg btnSDB;
+   stopwatch stopwatch (.clk_1hz(clk_1hz), .rst(btnR), .enable(enable), .dig3(dig3), .dig2(dig2), .dig1(dig1), .dig0(dig0));
    
-   /*AUTOWIRE*/
-   // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire [seq_dp_width-1:0] seq_tx_data;         // From seq_ of seq.v
-   wire                 seq_tx_valid;           // From seq_ of seq.v
-   wire [7:0]           uart_rx_data;           // From uart_top_ of uart_top.v
-   wire                 uart_rx_valid;          // From uart_top_ of uart_top.v
-   wire                 uart_tx_busy;           // From uart_top_ of uart_top.v
-   // End of automatics
+   display_mux display_mux(.clk_100hz(clk_100hz), .dig3(dig3), .dig2(dig2), .dig1(dig1), .dig0(dig0), .an(an), .seg(seg));
    
-   wire        rst;
-   wire        arst_i;
-   wire [17:0] clk_dv_inc;
-
-   reg [1:0]   arst_ff;
-   reg [16:0]  clk_dv;
-   reg         clk_en;
-   reg         clk_en_d;
-      
-   reg [7:0]   inst_wd;
-   reg         inst_vld;
-   reg [2:0]   step_d;
-
-   reg [7:0]   inst_cnt;
+    always @ (posedge btnSDB) begin
+         enable <= ~enable;
+    end 
+    
+    always @(posedge clk_100hz) begin
+         btnSDB <= btnS;
+    end
+//   always @ (posedge clk) begin
+        
+//   end
    
-   // ===========================================================================
-   // Asynchronous Reset
-   // ===========================================================================
 
-   assign arst_i = btnR;
-   assign rst = arst_ff[0];
-   
-   always @ (posedge clk or posedge arst_i)
-     if (arst_i)
-       arst_ff <= 2'b11;
-     else
-       arst_ff <= {1'b0, arst_ff[1]};
-
-   // ===========================================================================
-   // 763Hz timing signal for clock enable
-   // ===========================================================================
-
-   assign clk_dv_inc = clk_dv + 1;
-   
-   always @ (posedge clk)
-     if (rst)
-       begin
-          clk_dv   <= 0;
-          clk_en   <= 1'b0;
-          clk_en_d <= 1'b0;
-       end
-     else
-       begin
-          clk_dv   <= clk_dv_inc[16:0];
-          clk_en   <= clk_dv_inc[17];
-          clk_en_d <= clk_en;
-       end
-   
-   // ===========================================================================
-   // Instruction Stepping Control
-   // ===========================================================================
-
-   always @ (posedge clk)
-     if (rst)
-       begin
-          inst_wd[7:0] <= 0;
-          step_d[2:0]  <= 0;
-       end
-     else if (clk_en)
-       begin
-          inst_wd[7:0] <= sw[7:0];
-          step_d[2:0]  <= {btnS, step_d[2:1]};
-       end
-
-   always @ (posedge clk)
-     if (rst)
-       inst_vld <= 1'b0;
-     else
-       inst_vld <= ~step_d[0] & step_d[1] & clk_en_d;
-
-   always @ (posedge clk)
-     if (rst)
-       inst_cnt <= 0;
-     else if (inst_vld)
-       inst_cnt <= inst_cnt + 1;
-
-   assign led[7:0] = inst_cnt[7:0];
-   
-   // ===========================================================================
-   // Sequencer
-   // ===========================================================================
-
-   seq seq_ (// Outputs
-             .o_tx_data                 (seq_tx_data[seq_dp_width-1:0]),
-             .o_tx_valid                (seq_tx_valid),
-             // Inputs
-             .i_tx_busy                 (uart_tx_busy),
-             .i_inst                    (inst_wd[seq_in_width-1:0]),
-             .i_inst_valid              (inst_vld),
-             /*AUTOINST*/
-             // Inputs
-             .clk                       (clk),
-             .rst                       (rst));
-   
-   // ===========================================================================
-   // UART controller
-   // ===========================================================================
-
-   uart_top uart_top_ (// Outputs
-                       .o_tx            (RsTx),
-                       .o_tx_busy       (uart_tx_busy),
-                       .o_rx_data       (uart_rx_data[7:0]),
-                       .o_rx_valid      (uart_rx_valid),
-                       // Inputs
-                       .i_rx            (RsRx),
-                       .i_tx_data       (seq_tx_data[seq_dp_width-1:0]),
-                       .i_tx_stb        (seq_tx_valid),
-                       /*AUTOINST*/
-                       // Inputs
-                       .clk             (clk),
-                       .rst             (rst));
-   
 endmodule // basys3
 // Local Variables:
 // verilog-library-flags:("-f ../input.vc")
